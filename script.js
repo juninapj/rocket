37d93d0747224e43e65f77380e0570a151976a63
Bruno Bernardo <jbernardojunioor@gmail.com>
2024-11-13 10:05:21 -0300

    :tada: initial commit

---
+++ script.js
@@ -0,0 +1,24 @@
+/*
+  if (html.classList.contains("light")) {
+    html.classList.remove("light")
+  } else {
+    html.classList.add("light")
+  }
+    */
+
+function toggleMode() {
+  const html = document.documentElement
+  html.classList.toggle("light")
+
+  /*pega a tag img*/
+  const img = document.querySelector("#profile img")
+
+  /*substituir a imagem*/
+  if (html.classList.contains("light")) {
+    /* se tiver light mode, adicionar a imagem light*/
+    img.setAttribute("src", "./assets/avatar-light.png")
+    /* se tiver sem o light mode, manter a imagem */
+  } else {
+    img.setAttribute("src", "./assets/avatar.png")
+  }
+}
